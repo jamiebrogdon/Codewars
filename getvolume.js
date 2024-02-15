05bd992487a109b22fd607a09f84d37fdd12c64f
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-02-14 23:42:45 -0600

    Create getvolume.js

---
+++ getvolume.js
@@ -0,0 +1,16 @@
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
+//after googling the equation for finding volume in an object...
+//and realizing I am unfamiliar with the syntax above for a function...
+//I went with my gut instict and did a return with the equation...
+//bam it worked. score.
+
+//however, this is the code I wanted to write that wasn't accepted but works:
+
+function getVolumeOfCubiod(length, width, height) {
+  return length * width * height
+}
+getVolumeOfCubiod(//put your three numbers - arguments here//)
