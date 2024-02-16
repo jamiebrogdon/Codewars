e293e80e682c01a6bdcd8ec0e7b1ae06f961f122
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-02-16 10:54:21 -0600

    Create toUpperCase.js

---
+++ toUpperCase.js
@@ -0,0 +1,11 @@
+/*Write a function which converts the input string to uppercase:
+
+SOLUTION:
+
+*/
+
+function makeUpperCase(str) {
+   return str.toUpperCase();
+}
+
+//used the 'toUpperCase' method to return the input as all caps
