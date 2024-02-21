dc4fb6dd5024b18c1d0c1cb338b65ec4bb679d8d
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-02-20 23:13:47 -0600

    Create numberToString.js

---
+++ numberToString.js
@@ -0,0 +1,9 @@
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+
+
+// the .toString method is used to take a number and transform it into a string! 
+
+function numberToString(num) {
+  return num.toString();
+}
