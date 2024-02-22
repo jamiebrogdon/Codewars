eabb680d2921d2fa4e69b183da498f75cd8aab95
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-02-21 23:26:23 -0600

    Create multiply.js

---
+++ multiply.js
@@ -0,0 +1,14 @@
+//This code does not execute properly. Try to figure out why.
+
+//THE PROBLEM:
+
+function multiply(a, b){
+  a * b
+}
+
+// SOLUTION
+// needed a return or console.log to return value from function:
+
+function multiply(a, b){
+  return a * b;
+}
